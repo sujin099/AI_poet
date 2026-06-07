978c9b845337cc4c87e297f3aa2986c7538ddc30
sujin099 <ssjssj0608@gmail.com>
2026-06-07 19:39:50 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ chat_model = ChatOpenAI()
 
 # subject = "AI"
 # result = chat_model.invoke(subject + "AI에 대한 시를 써줘")
-# print(reult.content)
+# print(result.content)
 
 import streamlit as st
 
 st.title("인공지능 시인")
 subject = st.text_input("시의 주제를 입력해주세요.")
-st.write("시의 주제 :" + subject)
+st.write("시의 주제는 :" + subject)
 
 if st.button("시 작성"):
-    with st.spinner("시 작성 중 ..."):
-        result = chat_model.invoke(suject + "에 대한 시를 써줘")
+    with st.spinner("시 작성중 ..."):
+        result = chat_model.invoke(subject + "에 대한 시를 써줘")
         st.write(result.content)
